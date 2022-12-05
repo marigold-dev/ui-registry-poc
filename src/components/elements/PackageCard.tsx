e1e0be0c9ffc3e6147a2bb57d2079c7d0928fb78
JulesGuesnon <jules.guesnon@hetic.net>
2022-12-05 16:27:36 +0100

    fix(UI): Fix card to take full height

--- src/components/elements/PackageCard.tsx
+++ src/components/elements/PackageCard.tsx
@@ -8,7 +8,10 @@ type Props = {
 const PackageCard = ({ pkg }: Props) => {
   return (
     <div className="card rounded">
-      <Link className="has-text-black" href={`package/${pkg.name}`}>
+      <Link
+        className="has-text-black h-full flex flex-col"
+        href={`package/${pkg.name}`}
+      >
         <header className="card-header">
           <p className="card-header-title">
             {pkg.isFeatured && (
@@ -19,7 +22,7 @@ const PackageCard = ({ pkg }: Props) => {
             {pkg.name}
           </p>
         </header>
-        <div className="card-content">
+        <div className="card-content mb-auto">
           <div className="content">
             {!pkg.description ? (
               <span className="text-neutral-500">No description</span>
@@ -28,7 +31,7 @@ const PackageCard = ({ pkg }: Props) => {
             )}
           </div>
         </div>
-        <div className="card-footer">
+        <div className="card-footer mt-auto">
           <div className="card-footer-item">
             <span className="has-text-weight-light">
               <span className="has-text-weight-light ml-2">{pkg.version}</span>
